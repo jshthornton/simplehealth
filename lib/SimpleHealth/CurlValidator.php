a21087876e9738113d6ac650704a145c6236486b
Joshua Thornton <joshua@techinasia.com>
2015-10-07 10:25:56 +0800

    Fixed bad type hinting.

--- lib/SimpleHealth/CurlValidator.php
+++ lib/SimpleHealth/CurlValidator.php
@@ -3,12 +3,13 @@ namespace SimpleHealth;
 
 use \SimpleHealth\ValidatorInterface as ValidatorInterface;
 use \SimpleHealth\ValidatorResult as ValidatorResult;
+use \GuzzleHttp\Psr7\Response;
 
 class CurlValidator implements ValidatorInterface {
 	function __construct() {
 	}
 
-	public function isValid(\Guzzle\ResponseInterface $data) {
+	public function isValid(\GuzzleHttp\Psr7\Response $data) {
 		$http_status = $data->getStatusCode();
 
 		if($http_status === 200) {
