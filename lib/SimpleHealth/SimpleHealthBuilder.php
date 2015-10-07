f577087b37022a922c5ca163f19f3bab256aa404
Joshua Thornton <joshua@techinasia.com>
2015-10-07 17:29:52 +0800

    Builder now formats the urls prior to construction.

--- lib/SimpleHealth/SimpleHealthBuilder.php
+++ lib/SimpleHealth/SimpleHealthBuilder.php
@@ -3,6 +3,8 @@ namespace SimpleHealth;
 
 use \SimpleHealth\SimpleHealth as SimpleHealth;
 use \SimpleHealth\NodeHealthCheckFactory as NodeHealthCheckFactory;
+use \ValueObjects\Structure\Collection as Collection;
+use \ValueObjects\Web\Url as Url;
 
 class SimpleHealthBuilder {
 	public $endpoints;
@@ -12,8 +14,14 @@ class SimpleHealthBuilder {
 	}
 
 	public function build() {
+		$endpoints = \SplFixedArray::fromArray($this->endpoints);
+		for($i = 0, $len = count($endpoints); $i < $len; $i++) {
+			$endpoints[$i] = Url::fromNative($endpoints[$i]);
+		}
+		$endpoints = new Collection($endpoints);
+
 		$node_healthcheck_factory = new NodeHealthCheckFactory();
-		$node_healthcheck = $node_healthcheck_factory->make($this->endpoints);
-		return new SimpleHealth($this->endpoints, $node_healthcheck);
+		$node_healthcheck = $node_healthcheck_factory->make($endpoints);
+		return new SimpleHealth($endpoints, $node_healthcheck);
 	}
 }
