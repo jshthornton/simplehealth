497db243e97b01e4d55e8f9b53df39efd2e3ce02
Joshua Thornton <joshua@techinasia.com>
2015-10-08 16:25:21 +0800

    Fixed so that the unit test does not actually perform a request.

--- tests/unit/CurlMechanismTest.php
+++ tests/unit/CurlMechanismTest.php
@@ -2,13 +2,17 @@
 use \SimpleHealth\CurlMechanism as CurlMechanism;
 use \ValueObjects\Web\Url as Url;
 use \GuzzleHttp\Client;
+use \Mockery;
 
 class CurlMechanismTest extends PHPUnit_Framework_TestCase {
   public function testCheckReturnsCode() {
-    $client = new GuzzleHttp\Client();
+  	$client_mock = \Mockery::mock('\GuzzleHttp\Client');
+  	$response_mock = \Mockery::mock('\GuzzleHttp\Psr7\Response');
     $url = Url::fromNative('http://www.php.net');
 
-    $obj = new \SimpleHealth\CurlMechanism($client, $url);
+    $client_mock->shouldReceive('request')->andReturn($response_mock);
+
+    $obj = new \SimpleHealth\CurlMechanism($client_mock, $url);
     $code = $obj->request();
   }
 }
